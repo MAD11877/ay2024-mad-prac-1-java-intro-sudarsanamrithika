dbcceb8198a0a952ea6d68a7915336ceb3961030
sudarsanamrithika <veethe2nd@gmail.com>
2024-04-16 11:44:16 +0800

    IM FREE

--- Question5.java
+++ Question5.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.util.HashMap;
 
 public class Question5
 {
@@ -28,5 +29,33 @@ public class Question5
      
     Scanner in = new Scanner(System.in);
     
+      //count of number user will input 
+      int numIntegers = in.nextInt();   
+
+      HashMap<Integer, Integer> frequencyMap = new HashMap<Integer, Integer>(); 
+      int maxFreq = 0; 
+      int mode = 0; 
+
+      //prompt to enter integers 
+      for (int i = 0; i < numIntegers; i++) { 
+        int current = in.nextInt();  // Read each integer 
+        // Update frequency count for every integer 
+        if (frequencyMap.containsKey(current)) { 
+          frequencyMap.put(current, frequencyMap.get(current) + 1); 
+        } else { 
+          frequencyMap.put(current, 1); 
+        } 
+
+        // Check if current integer has a higher frequency 
+        if (frequencyMap.get(current) > maxFreq) { 
+          maxFreq = frequencyMap.get(current); 
+          mode = current; 
+        } 
+      } 
+
+      // Output mode 
+      System.out.print(mode); 
+      in.close(); 
+    }
   }
 }
